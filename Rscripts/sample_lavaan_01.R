2a6627397c2255f746f5530767fedc6c9b70f872
2waybene <jianying.li@gmail.com>
2018-02-07 16:27:02 -0500

    add composite

--- Rscripts/sample_lavaan_01.R
+++ Rscripts/sample_lavaan_01.R
@@ -3,7 +3,7 @@
 # Set working directory and load data
 setwd("X:/myGit/SEMproject/data") #commands in bold
 dat <- read.csv("GSE58144_levels_activities.csv")
-
+library(lavaan)
 
 
 # Examine 
@@ -11,23 +11,32 @@ summary(dat)
 
 
 # Step 1: Specify model
-mod.1 <- 'SOX17_lev.2 ~ GATA2_lev + ~ PGR_lev'
+mod.1 <- 'SOX17_lev.2 ~ GATA2_lev +  PGR_lev'
 
 # Step 1: Specify another model
 # How to capture a bi-directional relationship in the model??
- 
-mod.2 <- 'SOX17_lev.2 ~ GATA2_lev + ~ PGR_lev
-GATA2_lev ~ PGR_lev'
-
-#PGR_lev ~ GATA2_lev'
-
 # Step 2: Estimate model
 mod.1.fit <- sem(mod.1, data=dat)
-
-# Check variances
 varTable(mod.1.fit)
 
 # Step 3: Extract results
 summary(mod.1.fit) 
+
+summary(mod.1.fit, rsq=T, standardized=T) 
+
+##  second model 
+mod.2 <- 'SOX17_lev.2 <~ 1*GATA2_lev +  PGR_lev'
+
+mod.2.fit <- sem(mod.2, data=dat)
+# Check variances
+varTable(mod.2.fit)
+
+# Step 3: Extract results
+summary(mod.2.fit) 
+#PGR_lev ~ GATA2_lev'
+
+
+mod.2.fit <- sem(mod.2, data=dat)
+
 
 
